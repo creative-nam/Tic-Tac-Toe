028d2711159678368e6321068fff7bb980357bd6
creative-username <myc82570@gmail.com>
2022-10-04 22:12:47 +0200

    Set up Game class, and define messages to display throughout the game
    Created the game class which will be responsible for connecting all
    existing classes to execute out main game loop.
    
    Defined some of the messages to be displayed throughout the game, like
    'new game', 'new round', etc.
    
    Also wrote a method that allows us to clear the terminal, which will be
    useful during the game to separate the rounds.

---
+++ game.rb
@@ -0,0 +1,51 @@
+require_relative 'board'
+require_relative 'player'
+
+class Game
+  private
+
+  attr_accessor :player1, :player2, :board
+
+  public
+
+  def initialize
+    self.player1 = Player.new
+    self.player2 = Player.new
+
+    self.board = Board.new(5)
+  end
+
+  private
+
+  def announce_game_start
+    puts_with_padding('=', 'NEW GAME INITIATED')
+  end
+
+  def announce_new_round(round)
+    puts_with_padding('=', "Round #{round}")
+  end
+
+  def announce_game_over
+    puts_with_padding '=', 'GAME OVER'
+  end
+
+  def clear_terminal
+    system('cls') || system('clear')
+  end
+
+  def puts_with_padding(decorator_symbol, msg)
+    10.times { print " #{decorator_symbol} " }
+    print msg
+    10.times { print " #{decorator_symbol} " }
+
+    puts ''
+  end
+
+  def toggle_current_player(current_player)
+    current_player == player1 ? player2 : player1
+  end
+end
+
+game = Game.new
+
+game.start
