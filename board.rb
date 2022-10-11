d85db4e7785ca6709a9e40686159bbff3c0373c7
creative-username <myc82570@gmail.com>
2022-10-12 00:04:45 +0200

    Extracted some of the location logic back to the board class
    Extracted the location logic which was only being used in the context of
    the board (and not for user input validation) back to the Board class.
    
    This is so that our Location module will remain only with user-input
    validation logic/methods.

--- board.rb
+++ board.rb
@@ -1,10 +1,8 @@
 require_relative 'helper_methods/board_dimension'
 require_relative 'helper_methods/board_representation'
-require_relative 'helper_methods/location'
 require_relative 'winning_logic'
 
 class Board
-  include Location
   include WinningLogic
   include BoardDimension
 
@@ -62,7 +60,43 @@ class Board
     self.available_locations = available_locations
   end
 
+  def populate_grid(grid)
+    populated_grid = grid
+    location_number = 1
+
+    populated_grid.each_with_index do |row, row_index|
+      row.each_with_index do |col, col_index|
+        populated_grid[row_index][col_index] = location_number
+
+        location_number += 1
+      end
+    end
+
+    populated_grid
+  end
+
+  def generate_locations_map(grid)
+    locations_map = {}
+    location = 1
+
+    grid.each_with_index do |row, row_index|
+      row.each_with_index do |_col, col_index|
+        locations_map[location] = [row_index, col_index]
+
+        location += 1
+      end
+    end
+
+    locations_map
+  end
+
   def convert_index_to_location(grid_index)
-    super(locations_map, grid_index)
+    equivalent_location = nil
+
+    locations_map.each do |location, index|
+      equivalent_location = location if grid_index == index
+    end
+
+    equivalent_location
   end
 end
